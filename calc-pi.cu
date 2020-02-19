7f5539b7051744eb41dc6338450d04e3b195e642
AuthorSkN <author.skn@gmail.com>
2020-02-20 01:20:04 +0400

    PI calculation with GPU

--- calc-pi.cu
+++ calc-pi.cu
@@ -5,23 +5,61 @@
 #include <time.h>
 #include <curand_kernel.h>
 
+#define TRIALS_PER_THREAD 1024
+#define BLOCKS 256
+#define THREADS 256
 
-float cpuPICalculate(long trials) {
+
+__global__ void gpuPiCalculate(float *estimate, curandState *states) {
+	unsigned int tid = threadIdx.x + blockDim.x * blockIdx.x;
+	int pointsInCircle = 0;
+	float x, y;
+
+	curand_init(5, tid, 0, &states[tid]);  //initialize curand
+
+	for (int i = 0; i < TRIALS_PER_THREAD; i++) {
+		x = curand_uniform(&states[tid]);
+		y = curand_uniform(&states[tid]);
+		pointsInCircle += (x*x + y * y <= 1.0f); 
+	}
+	estimate[tid] = 4.0f * pointsInCircle / (float)TRIALS_PER_THREAD;
+}
+
+float cpuPiCalculate(long trials) {
 	float x, y;
 	long pointsInCircle = 0;
 	for (long i = 0; i < trials; i++) {
 		x = rand() / (float)RAND_MAX;
 		y = rand() / (float)RAND_MAX;
-		pointsInCircle += (x*x + y * y <= 1.0f);
+		pointsInCircle += (x * x + y * y <= 1.0f);
 	}
 	return 4.0f * pointsInCircle / trials;
 }
 
 int main(int argc, char *argv[]) {
 	clock_t start, stop;
+	float host[BLOCKS * THREADS];
+	float *dev;
+	curandState *devStates;
 
 	start = clock();
-	float cpuPI = cpuPICalculate(100000000);
+	cudaMalloc((void **)&dev, BLOCKS * THREADS * sizeof(float)); 
+	cudaMalloc((void **)&devStates, THREADS * BLOCKS * sizeof(curandState));
+
+	gpuPiCalculate << <BLOCKS, THREADS >> > (dev, devStates);
+
+	cudaMemcpy(host, dev, BLOCKS * THREADS * sizeof(float), cudaMemcpyDeviceToHost);
+	float gpuPI = 0;
+	for (int i = 0; i < BLOCKS * THREADS; i++) {
+		gpuPI += host[i];
+	}
+	gpuPI /= (BLOCKS * THREADS);
+	stop = clock();
+	printf("GPU PI= %f\n", gpuPI);
+	printf("GPU PI calculate time %f s.\n", (stop - start) / (float)CLOCKS_PER_SEC);
+	
+	start = clock();
+	float cpuPI = cpuPiCalculate(BLOCKS * THREADS * TRIALS_PER_THREAD);
 	stop = clock();
 	printf("CPU PI= %f\n", cpuPI);
 	printf("CPU PI calculate time %f s.\n", (stop - start) / (float)CLOCKS_PER_SEC);
